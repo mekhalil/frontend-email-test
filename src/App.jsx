179ef8eb7ca9eb0ea6c1d808428b2e4e2ba88411
khalilm <khalil.merzouki@knockoutgaming.com>
2018-06-11 01:22:11 +0200

    main app and some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 // @flow
 
 import React, { Component } from 'react';
-import EmailInputField from './EmailInputField';
-import isValidEmail from './utils';
+import { HashRouter, Route } from 'react-router-dom';
+import Home from './Home';
+import Result from './Result';
 
 type EmailType = {
     value: string,
@@ -10,7 +11,7 @@ type EmailType = {
     required: boolean
 };
 
-type State = {
+type ResultEmailType = {
     toEmail: EmailType,
     ccEmail: EmailType,
     bccEmail: EmailType,
@@ -19,203 +20,22 @@ type State = {
     imagePreviewUrl: any[]
 };
 
-type Props = {};
+type State = {
+    email: ResultEmailType
+}
 
-class App extends Component<Props, State> {
-    state = {
-        toEmail: {
-            value: '',
-            validStatus: true,
-            required: true
-        },
-        ccEmail: {
-            value: '',
-            validStatus: true,
-            required: false
-        },
-        bccEmail: {
-            value: '',
-            validStatus: true,
-            required: false
-        },
-        subject: '',
-        message: '',
-        imagePreviewUrl: []
+class App extends Component<{}, State> {
+    sendEmail = (email: ResultEmailType) => {
+        this.setState({ email });
     };
-
-    getTheImgData(file: File) {
-        const reader = new FileReader();
-        reader.onloadend = () => {
-            const imagePreviewUrl = [
-                ...this.state.imagePreviewUrl,
-                { value: reader.result, id: file.name.replace(/\s/g, '') }
-            ];
-            this.setState({
-                imagePreviewUrl
-            });
-        };
-
-        reader.readAsDataURL(file);
-    }
-
-    handleInputChange = (
-        event: SyntheticKeyboardEvent<HTMLFormElement> & { target: HTMLInputElement }
-    ) => {
-        event.preventDefault();
-        if (event.target.type === 'email') {
-            const emailData = {};
-            emailData.value = event.target.value;
-            if (!this.state[event.target.name].required && !emailData.value) {
-                emailData.validStatus = true;
-            } else {
-                emailData.validStatus = isValidEmail(emailData.value);
-            }
-            this.setState({ [event.target.name]: emailData });
-        } else {
-            this.setState({ [event.target.name]: event.target.value });
-        }
-    };
-
-    sendEmail = (event: SyntheticEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        // const { toEmail, ccEmail, bccEmail, subject, message } = this.state;
-        // console.log('To: ', toEmail.value);
-        // console.log('ccEmail: ', ccEmail.value);
-        // console.log('bccEmail: ', bccEmail.value);
-        // console.log('subject: ', subject);
-        // console.log('message: ', message);
-    };
-
-    checkIfCanSubmit = () => {
-        const { toEmail, ccEmail, bccEmail, subject } = this.state;
-        return !!(
-            subject &&
-            toEmail.value &&
-            (toEmail.validStatus && ccEmail.validStatus && bccEmail.validStatus)
-        );
-    };
-
-    uploadMultiFiles = (event: SyntheticEvent<EventTarget>) => {
-        event.preventDefault();
-        const fileUploadElem = document.getElementById('fileid');
-        if (fileUploadElem) {
-            fileUploadElem.click();
-        }
-    };
-
-    handleUploadedFiles = (event: SyntheticEvent<EventTarget>) => {
-        event.preventDefault();
-        const fileUploadElem: HTMLInputElement = document.getElementById('fileid');
-        const fileList: FileList = fileUploadElem.files;
-
-        for (let i = 0; i < fileList.length; i += 1) {
-            this.getTheImgData(fileList[i]);
-        }
-    };
-
-    removeImage = (event: SyntheticEvent<EventTarget>) => {
-        event.preventDefault();
-        const imagePreviewUrl = this.state.imagePreviewUrl.filter(file => file.id !== event.target.id);
-        this.setState({imagePreviewUrl})
-    };
-
     render() {
-        const toEmail = {
-            id: 'emailToInput',
-            name: 'toEmail',
-            placeholder: 'To',
-            multiple: true,
-            value: this.state.toEmail.value,
-            validationStatus: this.state.toEmail.validStatus,
-            handleOnChange: this.handleInputChange
-        };
-        const ccEmail = {
-            id: 'emailCcInput',
-            name: 'ccEmail',
-            placeholder: 'CC',
-            multiple: true,
-            value: this.state.ccEmail.value,
-            validationStatus: this.state.ccEmail.validStatus,
-            handleOnChange: this.handleInputChange
-        };
-        const bccEmail = {
-            id: 'emailBccInput',
-            name: 'bccEmail',
-            placeholder: 'BCC',
-            multiple: true,
-            value: this.state.bccEmail.value,
-            validationStatus: this.state.bccEmail.validStatus,
-            handleOnChange: this.handleInputChange
-        };
-        const canSubmit = this.checkIfCanSubmit();
         return (
-            <div className="container">
-                <header className="header">
-                    <h5 className="header__title">Send E-mail</h5>
-                </header>
-                <div className="content">
-                    <form id="form" onSubmit={this.sendEmail}>
-                        <EmailInputField {...toEmail} />
-                        <EmailInputField {...ccEmail} />
-                        <EmailInputField {...bccEmail} />
-                        <input
-                            type="text"
-                            id="subject"
-                            className="text-input-field"
-                            name="subject"
-                            value={this.state.subject}
-                            placeholder="Subject"
-                            onChange={this.handleInputChange}
-                        />
-                        <textarea
-                            id="message"
-                            className="text-input-field"
-                            name="message"
-                            value={this.state.message}
-                            placeholder="Message"
-                            onChange={this.handleInputChange}
-                        />
-                        <input
-                            id="fileid"
-                            type="file"
-                            onChange={this.handleUploadedFiles}
-                            multiple
-                            hidden
-                        />
-                    </form>
-                    {this.state.imagePreviewUrl.length > 0 ? <h4>Attached files</h4> : ''}
-                    <div className="image-preview-list">
-                        {this.state.imagePreviewUrl.map(url => (
-                            <div className="image-preview-wrapper">
-                                <button className="delete-btn" id={url.id} onClick={this.removeImage}>
-                                    <i className="fas fa-trash-alt" />
-                                </button>
-                                <img
-                                    className="preview-image"
-                                    src={url.value}
-                                    key={url.id}
-                                    alt=""
-                                />
-                            </div>
-                        ))}
-                    </div>
-                    <div className="form-footer">
-                        <button className="upload-file-btn" onClick={this.uploadMultiFiles}>
-                            <i className="fas fa-paperclip" />
-                        </button>
-                        <button
-                            className="submit-btn"
-                            type="submit"
-                            form="form1"
-                            value="Submit"
-                            disabled={!canSubmit}
-                        >
-                            <i className="fas fa-arrow-right" />
-                            <span className="submit-btn__text">Send</span>
-                        </button>
-                    </div>
+            <HashRouter>
+                <div className="app">
+                    <Route exact path="/" component={props => <Home {...props} sendEmail={this.sendEmail} />} />
+                    <Route exact path="/result" component={() => <Result {...this.state} />} />
                 </div>
-            </div>
+            </HashRouter>
         );
     }
 }
---
+++ src/Result.jsx
@@ -0,0 +1,63 @@
+// @flow
+
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+type EmailType = {
+    value: string,
+    validStatus: boolean,
+    required: boolean
+};
+
+type Props = {
+    email: {
+        toEmail: EmailType,
+        ccEmail: EmailType,
+        bccEmail: EmailType,
+        subject: string,
+        message: string,
+        imagePreviewUrl: any[]
+    }
+};
+
+function Result(props: Props) {
+    if (!Object.getOwnPropertyNames(props).length) {
+        return (
+            <div>
+                <h1>Go to Home page First</h1>
+                <Link to="/">back</Link>
+            </div>
+        )
+    }
+    const imagePreviewUrlList = Array.isArray(props.email.imagePreviewUrl) ? props.email.imagePreviewUrl : [];
+    return (
+        <div className="result">
+            <header className="result-header">
+                <i className="fas fa-check-circle" />
+                <h5 className="result-header__title">Your email has been sent</h5>
+            </header>
+            <hr/>
+            <div className="content">
+                <h4 className="email-subject">{props.email.subject}</h4>
+                <span className="email-reciever">to {props.email.toEmail ? props.email.toEmail.value : ''}</span>
+                <p className="message">{props.email.message}</p>
+            </div>
+            <div className="footer">
+                <div className="image-preview-list">
+                    {imagePreviewUrlList.map(url => (
+                        <div className="image-preview-wrapper" key={url.id}>
+                            <img
+                                className="preview-image"
+                                src={url.value}
+                                alt=""
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <Link to="/">back</Link>
+        </div>
+    )
+}
+
+export default Result;
