5098936ed2f5d966273617d97c1e1333bb223f38
Khalil Merzouki <khalilmerzouki@MacBook-Pro-de-Khalil.local>
2018-06-13 00:32:29 +0200

    Splitting the webpack config to dev and production

---
+++ webpack.common.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+module.exports = {
+    context: __dirname,
+    entry: [
+        './src/clientApp.jsx'
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/'
+    },
+        resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.jsx?$/,
+                loader: 'eslint-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,15 @@
-const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
 
-module.exports = {
-    context: __dirname,
-    entry: [
-        './src/clientApp.jsx'
-    ],
+module.exports = merge(common, {
+    mode: 'development',
     devtool: 'inline-source-map',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public'),
-        publicPath: '/public/'
-    },
     devServer: {
-        publicPath: '/public/'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.json']
+        publicPath: '/dist/'
     },
     stats: {
         colors: true,
         reasons: true,
         chunks: true
-    },
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /\.jsx?$/,
-                loader: 'eslint-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            }
-        ]
-    },
-    mode: "development"
-};
+    }
+});
